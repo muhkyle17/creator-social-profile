61f1af91030794b936ab691ffa5dbff27ec725c5
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-27 14:23:25 -0800

    Add some additional border radius

--- src/app/components/SocialProfilePreview.jsx
+++ src/app/components/SocialProfilePreview.jsx
@@ -1,7 +1,7 @@
 const SocialProfilePreview = () => {
   return (
     <div className='bg-white h-full flex items-center justify-center'>
-      <div className='min-h-[47rem] w-[350px] mx-auto flex items-center justify-center rounded-3xl ring-8 ring-black py-6 px-6'>
+      <div className='min-h-[47rem] w-[350px] mx-auto flex items-center justify-center rounded-[3.5rem] ring-8 ring-black py-6 px-6'>
         This is where the content will go
       </div>
     </div>
