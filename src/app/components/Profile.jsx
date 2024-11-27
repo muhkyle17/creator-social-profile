0665d2772b3f0601c1d16be3ef9c3d53fab235ac
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-26 18:34:41 -0800

    Add initial design input

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -1,8 +1,23 @@
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-      <h2>A brief introduction about yourself</h2>
+    <div className='flex flex-row gap-10'>
+      <div className='w-[20%]'>
+        <h1 className='font-bold text-lg'>Profile</h1>
+        <h2 className='opacity-60'>A brief introduction about yourself</h2>
+      </div>
+      <div className='w-[40%] bg-white p-6 rounded-lg drop-shadow-lg'>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='name' className='text-sm'>
+            Name
+          </label>
+          <input
+            aria-label='Name Input'
+            type='text'
+            name='name'
+            className='border border-opacity-20 border-black rounded-md p-3 text-xs'
+          />
+        </div>
+      </div>
     </div>
   )
 }
