988aca0edaca00cf30163f67dc1ad564652eded6
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-27 00:30:48 -0800

    Add the photo url, and description

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -5,14 +5,37 @@ const Profile = () => {
         <h1 className='font-bold text-lg'>Profile</h1>
         <h2 className='opacity-60'>A brief introduction about yourself</h2>
       </div>
-      <div className='w-[40%] bg-white p-6 rounded-lg drop-shadow-lg'>
+      <div className='w-[40%] bg-white p-6 rounded-lg drop-shadow-lg flex flex-col gap-5'>
         <div className='flex flex-col gap-1'>
-          <label htmlFor='name' className='text-sm'>
+          <label htmlFor='name' className='text-sm text-gray-700'>
             Name
           </label>
           <input
             aria-label='Name Input'
             type='text'
+            name='name'
+            className='border border-opacity-20 border-black rounded-md p-3 text-xs'
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='description' className='text-sm text-gray-700'>
+            Short description about yourself
+          </label>
+          <textarea
+            aria-label='Description Input'
+            rows='4'
+            type='text'
+            name='description'
+            className='border border-opacity-20 border-black rounded-md p-3 text-xs'
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='photoUrl' className='text-sm text-gray-700'>
+            Photo URL
+          </label>
+          <input
+            aria-label='Photo Url Image Input'
+            type='photoUrl'
             name='name'
             className='border border-opacity-20 border-black rounded-md p-3 text-xs'
           />
