86e801098fa0ce9d345ed2c075e0c596121d9847
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-28 00:20:04 -0800

    Slightly adjust widths

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -1,11 +1,11 @@
 const Profile = () => {
   return (
     <div className='p-8 flex flex-row gap-10 justify-around'>
-      <div className='w-[35%]'>
+      <div className='w-[30%]'>
         <h1 className='font-bold text-lg'>Profile</h1>
         <h2 className='opacity-60'>A brief introduction about yourself</h2>
       </div>
-      <div className='w-[60%] h-fit bg-white p-6 rounded-lg drop-shadow-lg flex flex-col gap-5'>
+      <div className='w-[65%] h-fit bg-white p-6 rounded-lg drop-shadow-lg flex flex-col gap-5'>
         <div className='flex flex-col gap-1'>
           <label htmlFor='name' className='text-sm text-gray-700'>
             Name
