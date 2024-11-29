db064de7797209a2bc2b5135c9fdb70e46acf6f3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-29 00:16:47 -0800

    Add the rest of the social links

--- src/app/components/SocialLinks.jsx
+++ src/app/components/SocialLinks.jsx
@@ -1,4 +1,14 @@
-import { FaFacebook, FaTwitter } from 'react-icons/fa'
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaGithub,
+  FaTelegram,
+  FaLinkedin,
+  FaYoutube,
+  FaWhatsapp,
+} from 'react-icons/fa'
+import { MdOutlineMailOutline } from 'react-icons/md'
 
 import SocialLink from './SocialLink'
 
@@ -9,10 +19,22 @@ const SocialLinks = () => {
         <h1 className='font-bold text-lg'>Social Links</h1>
         <h2 className='opacity-60'>Add your social media links</h2>
       </div>
-      <div className='w-[65%] h-fit bg-white p-6 rounded-lg drop-shadow-lg'>
+      <div className='w-[65%] h-fit bg-white p-6 rounded-lg drop-shadow-lg flex flex-col gap-8'>
         <div className='flex flex-row gap-10 justify-between'>
           <SocialLink inputName='facebook' labelName='Facebook' icon={<FaFacebook />} />
           <SocialLink inputName='twitter' labelName='Twitter' icon={<FaTwitter />} />
+        </div>
+        <div className='flex flex-row gap-10 justify-between'>
+          <SocialLink inputName='instagram' labelName='Instgram' icon={<FaInstagram />} />
+          <SocialLink inputName='github' labelName='Github' icon={<FaGithub />} />
+        </div>
+        <div className='flex flex-row gap-10 justify-between'>
+          <SocialLink inputName='email' labelName='Email' icon={<MdOutlineMailOutline />} />
+          <SocialLink inputName='youtube' labelName='Youtube' icon={<FaYoutube />} />
+        </div>
+        <div className='flex flex-row gap-10 justify-between'>
+          <SocialLink inputName='whatsapp' labelName='WhatsApp' icon={<FaWhatsapp />} />
+          <div className='invisible w-1/2' />
         </div>
       </div>
     </div>
