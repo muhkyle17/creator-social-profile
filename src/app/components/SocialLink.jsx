99b79d5f20191ac2f600dfc0a73653fb8d783c5e
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-29 00:03:52 -0800

    Add social link component

--- src/app/components/SocialLink.jsx
+++ src/app/components/SocialLink.jsx
@@ -1,5 +1,25 @@
-const SocialLink = () => {
-  return <div>Social Link</div>
+import { FaFacebook } from 'react-icons/fa'
+
+const SocialLink = ({ inputName, labelName, icon }) => {
+  return (
+    <div className='flex flex-col gap-2 w-1/2'>
+      <label htmlFor='inputName' className='text-sm text-gray-700'>
+        {labelName}
+      </label>
+      <div className='flex flex-row items-center w-full'>
+        <span className='py-2 px-3 rounded-md rounded-r-none border border-opacity-20 border-black'>
+          <FaFacebook className='text-xl' />
+          {/* {icon} */}
+        </span>
+        <input
+          aria-label='Social Icons'
+          type='text'
+          name={inputName}
+          className='bg-white p-2 text-sm rounded-md rounded-l-none border-l-0 border border-opacity-20 border-black w-full'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default SocialLink
--- src/app/components/SocialLinks.jsx
+++ src/app/components/SocialLinks.jsx
@@ -1,5 +1,7 @@
 import { FaFacebook } from 'react-icons/fa'
 
+import SocialLink from './SocialLink'
+
 const SocialLinks = () => {
   return (
     <div className='p-8 flex flex-row gap-10 justify-around'>
@@ -9,38 +11,8 @@ const SocialLinks = () => {
       </div>
       <div className='w-[65%] h-fit bg-white p-6 rounded-lg drop-shadow-lg'>
         <div className='flex flex-row gap-10 justify-between'>
-          <div className='flex flex-col gap-2 w-1/2'>
-            <label htmlFor='facebook' className='text-sm text-gray-700'>
-              Facebook
-            </label>
-            <div className='flex flex-row items-center w-full'>
-              <span className='py-2 px-3 rounded-md rounded-r-none border border-opacity-20 border-black'>
-                <FaFacebook className='text-xl' />
-              </span>
-              <input
-                aria-label='Facebook'
-                type='text'
-                name='facebook'
-                className='bg-white p-2 text-sm rounded-md rounded-l-none border-l-0 border border-opacity-20 border-black w-full'
-              />
-            </div>
-          </div>
-          <div className='flex flex-col gap-2 w-1/2'>
-            <label htmlFor='facebook' className='text-sm text-gray-700'>
-              Facebook
-            </label>
-            <div className='flex flex-row items-center w-full'>
-              <span className='py-2 px-3 rounded-md rounded-r-none border border-opacity-20 border-black'>
-                <FaFacebook className='text-xl' />
-              </span>
-              <input
-                aria-label='Facebook'
-                type='text'
-                name='facebook'
-                className='bg-white p-2 text-sm rounded-md rounded-l-none border-l-0 border border-opacity-20 border-black w-full'
-              />
-            </div>
-          </div>
+          <SocialLink inputName='facebook' labelName='Facebook' icon='facebook' />
+          <SocialLink inputName='twitter' labelName='Twitter' icon='facebook' />
         </div>
         <div></div>
         <div></div>
