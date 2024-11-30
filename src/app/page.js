9ce19f96752f9e0ebbab589c9b440a17904caa98
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-29 18:33:41 -0800

    Add scroll logic

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,14 @@ const Divider = () => <hr className='bg-gray-500 h-[2px] mx-8' />
 export default function Home() {
   return (
     <div className='flex flex-row h-full min-h-screen'>
-      <div className='w-3/4 flex flex-col bg-foreground'>
+      <div className='w-3/4 flex flex-col bg-foreground overflow-y-auto min-h-screen h-screen'>
         <Profile />
         <Divider />
         <SocialLinks />
         <Divider />
         <Links />
       </div>
-      <div className='w-1/4'>
+      <div className='w-1/4 max-h-screen overscroll-none'>
         <SocialProfilePreview />
       </div>
     </div>
