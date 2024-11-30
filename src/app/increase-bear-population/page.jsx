3da7d3350377c21613811135eeb5f65301e68e01
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-30 13:33:44 -0800

    Add decrease population logic

--- src/app/increase-bear-population/page.jsx
+++ src/app/increase-bear-population/page.jsx
@@ -4,16 +4,23 @@ import useStore from '../hooks/useStore'
 
 const IncreaseBearPopulation = () => {
   const bears = useStore(state => state.bears)
-  console.log(bears, 'bears')
 
   const increasePopulation = useStore(state => state.increasePopulation)
 
+  const decreasePopulation = useStore(state => state.decreasePopulation)
+
   const removeAllBears = useStore(state => state.removeAllBears)
+
+  console.log(bears, 'bears')
 
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center gap-5'>
       <button className='bg-orange-400 px-5 py-2 rounded-full' onClick={increasePopulation}>
         Increase Population
+      </button>
+
+      <button className='bg-orange-300 px-5 py-2 rounded-full' onClick={decreasePopulation}>
+        Decrease Population
       </button>
 
       <button className='bg-orange-500 px-5 py-2 rounded-full' onClick={removeAllBears}>
