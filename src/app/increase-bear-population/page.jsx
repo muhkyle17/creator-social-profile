ccc23971e0ed625b5366cf54c414f95d08258374
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-30 13:28:21 -0800

    Add remove all bears logic

--- src/app/increase-bear-population/page.jsx
+++ src/app/increase-bear-population/page.jsx
@@ -8,10 +8,16 @@ const IncreaseBearPopulation = () => {
 
   const increasePopulation = useStore(state => state.increasePopulation)
 
+  const removeAllBears = useStore(state => state.removeAllBears)
+
   return (
-    <div className='w-screen h-screen flex items-center justify-center gap-5'>
+    <div className='w-screen h-screen flex flex-col items-center justify-center gap-5'>
       <button className='bg-orange-400 px-5 py-2 rounded-full' onClick={increasePopulation}>
         Increase Population
+      </button>
+
+      <button className='bg-orange-500 px-5 py-2 rounded-full' onClick={removeAllBears}>
+        Remove All Bears
       </button>
     </div>
   )
