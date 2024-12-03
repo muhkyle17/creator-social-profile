0893384728c64756be38d54742cb271e6bc031b1
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-12-03 12:45:21 -0800

    Add logic for updating and deleting links

--- src/app/hooks/linksDataStore.js
+++ src/app/hooks/linksDataStore.js
@@ -8,9 +8,23 @@ const linksDataStore = create(set => ({
       URL: '',
     },
   ],
-  setLinksData: newLink =>
+  setNewLink: newLink =>
     set(state => ({
       links: state.links.map(link => (link.id === newLink.id ? { ...link, ...newLink } : link)),
+    })),
+  setUpdateLink: updatedLink =>
+    set(state => ({
+      links: state.links.map(link =>
+        link.id === updatedLink.id ? { ...link, ...updatedLink } : link
+      ),
+    })),
+  setDeleteLink: deletedLink =>
+    set(state => ({
+      links: state.links.filter(link =>
+        link.id !== deletedLink.id
+          ? console.log(deletedLink.id, 'deletedLink.id')
+          : console.log(link.id, 'link.id')
+      ),
     })),
 }))
 
