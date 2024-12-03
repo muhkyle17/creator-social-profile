dbbfaaf239829a40315cb8a35de0223208068318
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-12-02 16:29:58 -0800

    Add buttons for each social media

--- src/app/components/SocialProfilePreview/SocialProfilePreview.jsx
+++ src/app/components/SocialProfilePreview/SocialProfilePreview.jsx
@@ -63,15 +63,60 @@ const SocialProfilePreview = () => {
               </div>
 
               <div className='flex flex-row gap-3 text-xl'>
-                <FaFacebook className='text-[#1877F2]' />
-                <FaTwitter className='text-[#1DA1F2]' />
-                <FaInstagram className='text-[#f77737]' />
-                <FaGithub />
-                <MdOutlineMailOutline />
-                <FaTelegram className='text-[#24A1DE]' />
-                <FaLinkedin className='text-[#0077B5]' />
-                <FaYoutube className='text-[#CD201F]' />
-                <FaWhatsapp className='text-[#4FCE5D]' />
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaTwitter className='text-[#1DA1F2]' />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaInstagram className='text-[#f77737]' />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaGithub />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <MdOutlineMailOutline />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaFacebook className='text-[#1877F2]' />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaTelegram className='text-[#24A1DE]' />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaLinkedin className='text-[#0077B5]' />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaYoutube className='text-[#CD201F]' />
+                </button>
+                <button
+                  type='button'
+                  className='cursor-pointer'
+                  onClick={() => console.log(`You clicked the ${'button'}`)}>
+                  <FaWhatsapp className='text-[#4FCE5D]' />
+                </button>
               </div>
             </div>
           </div>
