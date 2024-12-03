f906b11be31db8f28fdcd05fac782b41c6d440a8
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-12-02 16:47:36 -0800

    Move profile preview ui to new component

--- src/app/components/SocialProfilePreview/ProfilePreview.jsx
+++ src/app/components/SocialProfilePreview/ProfilePreview.jsx
@@ -1,5 +1,42 @@
-const ProfilePreview = () => {
-  return <div>Profile Preview</div>
+import Image from 'next/image'
+
+const ProfilePreview = ({ photoUrl, profileName, profileDescription }) => {
+  return (
+    <div className='flex flex-col gap-2 text-center items-center'>
+      <Image
+        alt='Temporary Image' // !Temporary Image
+        src={'https://uncommonschools.org/wp-content/uploads/2018/02/ryan-gosling-2-800x667.jpg'}
+        sizes='100vw'
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+        width={500}
+        height={300}
+        className='rounded-3xl'
+      />
+      <h1 className='text-2xl font-bold'>Ryan Gosling</h1>
+      <p className='text-lg opacity-60'>This is a temporary description</p>
+
+      {/* {photoUrl && (
+        <Image
+          alt='Profile Image'
+          src={photoUrl}
+          sizes='100vw'
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          width={500}
+          height={300}
+          className='rounded-3xl'
+        />
+      )} */}
+
+      {/* {profileName && <h1 className='text-2xl font-bold'>{profileName}</h1>}
+      {profileDescription && <p className='text-lg opacity-60'>{profileDescription}</p>} */}
+    </div>
+  )
 }
 
 export default ProfilePreview
--- src/app/components/SocialProfilePreview/SocialProfilePreview.jsx
+++ src/app/components/SocialProfilePreview/SocialProfilePreview.jsx
@@ -27,40 +27,11 @@ const SocialProfilePreview = () => {
         <div className='relative w-full min-h-[700px] max-h-[800px] flex flex-col gap-8 items-center justify-start'>
           <div className='absolute inset-0 overflow-auto pr-4 -mr-4'>
             <div className='flex flex-col gap-8 items-center'>
-              <div className='flex flex-col gap-2 text-center items-center'>
-                {photoUrl && (
-                  <Image
-                    alt='Profile Image'
-                    src={photoUrl}
-                    sizes='100vw'
-                    style={{
-                      width: '100%',
-                      height: 'auto',
-                    }}
-                    width={500}
-                    height={300}
-                    className='rounded-3xl'
-                  />
-                )}
-                <Image
-                  alt='Temporary Image' // !Temporary Image
-                  src={
-                    'https://uncommonschools.org/wp-content/uploads/2018/02/ryan-gosling-2-800x667.jpg'
-                  }
-                  sizes='100vw'
-                  style={{
-                    width: '100%',
-                    height: 'auto',
-                  }}
-                  width={500}
-                  height={300}
-                  className='rounded-3xl'
-                />
-                <h1 className='text-2xl font-bold'>Ryan Gosling</h1>
-                <p className='text-lg opacity-60'>This is a temporary description</p>
-                {/* <h1 className='text-2xl font-bold'>{profileName}</h1>
-                <p className='text-lg opacity-60'>{profileDescription}</p> */}
-              </div>
+              <ProfilePreview
+                profileName={profileName}
+                profileDescription={profileDescription}
+                photoUrl={photoUrl}
+              />
 
               <div className='flex flex-row gap-3 text-xl'>
                 <button
